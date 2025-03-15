e81215a3f6a8449b15ad4f9ae04555d0b99b2d49
istvan1243 <33933467+istvan1243@users.noreply.github.com>
2025-03-15 21:36:54 +0100

    null

--- src/Installer.php
+++ src/Installer.php
@@ -2,45 +2,20 @@
 
 namespace Istvan\ComposerFocusThemeInstaller;
 
-use Composer\Composer;
-use Composer\IO\IOInterface;
-use Composer\Plugin\PluginInterface;
 use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
-use Composer\Repository\InstalledRepositoryInterface;
 
-class Installer extends LibraryInstaller implements PluginInterface
+class Installer extends LibraryInstaller
 {
-    public function activate(Composer $composer, IOInterface $io)
+    public function supports($packageType)
     {
-        $composer->getInstallationManager()->addInstaller($this);
-    }
-
-    public function deactivate(Composer $composer, IOInterface $io)
-    {
-        // Nem kötelező implementálni, de a PluginInterface követelménye
-    }
-
-    public function uninstall(Composer $composer, IOInterface $io)
-    {
-        // A PluginInterface követelménye
-    }
-
-    public function uninstallPackage(InstalledRepositoryInterface $repo, PackageInterface $package)
-    {
-        // A LibraryInstaller követelménye
-        parent::uninstall($repo, $package);
+        return $packageType === 'focus-theme';
     }
 
     public function getInstallPath(PackageInterface $package)
     {
         $themeName = $this->getThemeName($package);
         return "app/Themes/{$themeName}";
-    }
-
-    public function supports($packageType)
-    {
-        return $packageType === 'focus-theme';
     }
 
     protected function getThemeName(PackageInterface $package)
