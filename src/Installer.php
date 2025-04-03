e87debae8aa4a48cb3a8fea2d681372bb7ca684e
istvan1243 <33933467+istvan1243@users.noreply.github.com>
2025-04-03 17:28:47 +0200

    null

--- src/Installer.php
+++ src/Installer.php
@@ -4,42 +4,69 @@ namespace Istvan\ComposerFocusThemeInstaller;
 
 use Composer\Composer;
 use Composer\IO\IOInterface;
-use Composer\Plugin\PluginInterface;
-use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\LibraryInstaller;
+use Composer\Package\PackageInterface;
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
 
-class Plugin implements PluginInterface, EventSubscriberInterface
+class Installer extends LibraryInstaller
 {
-    public function activate(Composer $composer, IOInterface $io)
+    public function getInstallPath(PackageInterface $package)
     {
-        $installer = new Installer($io, $composer);
-        $composer->getInstallationManager()->addInstaller($installer);
+        $themeName = $this->getThemeName($package);
+        return "Themes/{$themeName}";
     }
 
-    public function deactivate(Composer $composer, IOInterface $io)
+    public function supports($packageType)
     {
-        // Nincs szükség külön implementációra
+        return $packageType === 'focus-theme';
     }
 
-    public function uninstall(Composer $composer, IOInterface $io)
+    protected function getThemeName(PackageInterface $package)
     {
-        // Itt sem szükséges implementáció
+        $packageName = $package->getPrettyName();
+        $packageName = str_replace('istvan/', '', $packageName);
+
+        // Átalakítás PSR-4 kompatibilis névre
+        $themeName = str_replace('-', ' ', $packageName); // Kötőjelek helyett szóköz
+        $themeName = ucwords($themeName); // Minden szó első betűje nagybetű
+        $themeName = str_replace(' ', '', $themeName); // Szóközök eltávolítása
+
+        return $themeName;
     }
 
-    public static function getSubscribedEvents()
+    public static function postPackageInstall(PackageEvent $event)
     {
-        return [
-            'post-package-install' => 'postPackageInstall',
-            'post-package-uninstall' => 'postPackageUninstall',
-        ];
+        $package = $event->getOperation()->getPackage();
+        if ($package->getType() === 'focus-theme') {
+            $themeName = (new self($event->getIO(), $event->getComposer()))->getThemeName($package);
+            self::executeArtisanCommand($event->getIO(), "theme:setup {$themeName}");
+        }
     }
 
-    public function postPackageInstall(\Composer\Installer\PackageEvent $event)
+    public static function postPackageUninstall(PackageEvent $event)
     {
-        Installer::postPackageInstall($event);
+        $package = $event->getOperation()->getPackage();
+        if ($package->getType() === 'focus-theme') {
+            $themeName = (new self($event->getIO(), $event->getComposer()))->getThemeName($package);
+            self::executeArtisanCommand($event->getIO(), "theme:remove {$themeName}");
+        }
     }
 
-    public function postPackageUninstall(\Composer\Installer\PackageEvent $event)
+    protected static function executeArtisanCommand(IOInterface $io, $command)
     {
-        Installer::postPackageUninstall($event);
+        $cwd = getcwd();
+        $artisanPath = $cwd . '/artisan';
+
+        if (file_exists($artisanPath)) {
+            $io->write("Executing: php artisan {$command}");
+            system("php {$artisanPath} {$command}", $returnCode);
+
+            if ($returnCode !== 0) {
+                $io->writeError("Error executing artisan command: {$command}");
+            }
+        } else {
+            $io->writeError("Artisan file not found at: {$artisanPath}");
+        }
     }
 }
