814625759a44610735cf5585059734ead22f0dd8
istvan1243 <33933467+istvan1243@users.noreply.github.com>
2025-04-03 16:08:35 +0200

    null

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,6 +5,7 @@ namespace Istvan\ComposerFocusThemeInstaller;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Illuminate\Support\Facades\Artisan;
 
 class Plugin implements PluginInterface
 {
@@ -21,6 +22,21 @@ class Plugin implements PluginInterface
 
     public function uninstall(Composer $composer, IOInterface $io)
     {
-        // Itt sem szükséges implementáció
+        // Csomag neve alapján törli a téma beállításait
+        $package = $composer->getPackage();
+        $themeName = $this->getThemeName($package->getPrettyName());
+
+        // Laravel Artisan parancs meghívása
+        Artisan::call('theme:remove', ['theme' => $themeName]);
+
+        $io->write("Téma törölve: {$themeName}");
+    }
+
+    protected function getThemeName($packageName)
+    {
+        $packageName = str_replace('istvan/', '', $packageName);
+        $themeName = str_replace('-', ' ', $packageName);
+        $themeName = ucwords($themeName);
+        return str_replace(' ', '', $themeName);
     }
 }
